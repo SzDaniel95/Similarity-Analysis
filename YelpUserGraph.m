49aa6b770b22ab15cb9e888736f03b723795ae4f
Daniel <Daniel@Sztancsik>
2018-06-25 14:06:27 +0200

    User gráf létrehozás

---
+++ YelpUserGraph.m
@@ -0,0 +1,29 @@
+function [ G ] = YelpUserGraph()
+
+    %YelpUserGraph
+    %Datastore beolvasás
+    ds = datastore('D:/yelp_user_reduced.csv');
+    %1 sort olvasunk be egyszerre
+    ds.ReadSize=1;
+    G=graph();
+    %Add nodes and edges, 154/min -> Nagy számítási igény!!!
+    while hasdata(ds)
+        T = read(ds);
+        if string(T.friends)=='None'
+           %Ha nincsennek baratai csak létrehozzuk a csúcsot
+           if findnode(G,T.user_id)==0
+            G = addnode(G,T.user_id);
+           end
+        else
+           %Ha vannak baratai akkor léthezunk éleket
+           C = strsplit(string(T.friends),{', '});
+           for i=1:length(C)
+               G=addedge(G,T.user_id,cellstr(C(i)));
+           end
+        end
+    end
+    save('yelp_user_graph.mat','G')
+
+
+end
+
