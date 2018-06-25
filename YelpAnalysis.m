920b0c725b885f0c2fca07c220a1ab7bba087892
Daniel <Daniel@Sztancsik>
2018-06-25 13:31:45 +0200

    Javítás, Végeredmény mentése
    Nem kell előzetes hozzáadni csúcsot elég csak a while ciklusban

--- YelpAnalysis.m
+++ YelpAnalysis.m
@@ -1,21 +1,16 @@
 function [ G ] = YelpAnalysis()
 
-    %yelp_review
+    %YelpReviewGraph
     %Datastore beolvasás
     ds = datastore('D:/yelp_review_reduced.csv');
     %1 sort olvasunk be egyszerre
     ds.ReadSize=1;
     G=graph();
-    %Init node - otherwise: Error Graph does not contain node names.
-    T=read(ds);
-    G = addnode(G,T.business_id);
-    G = addnode(G,T.user_id);
-    %Add nodes and edges
+    %Add nodes and edges kb 100/sec sebesség kb 14 óra futtatás
     while hasdata(ds)
         T = read(ds);
         G = addedge(G,T.user_id,T.business_id,T.stars);
     end
-
-
+    save('yelp_review_graph.mat','G')
 end
 
