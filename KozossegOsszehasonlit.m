900f78cddbd65ff9788d0ff6001d3cf221e82262
Daniel <Daniel@Sztancsik>
2019-09-23 21:23:53 +0200

    Update KozossegOsszehasonlit.m

--- KozossegOsszehasonlit.m
+++ KozossegOsszehasonlit.m
@@ -1,24 +1,30 @@
-function [ result, name_pairs ] = KozossegOsszehasonlit()
-    
-    ds = datastore('C:/Users/User/Documents/Similarity-Analysis/kozossegek.csv');
-    T = readall(ds);
-    meret=size(T);
-    name_pairs=cell(53130,2);
-    result=zeros(53130,1);
-    z=1;
-    for i=1:meret(1)
-        for j=1:(meret(1))
-            if(i~=j)
-                name_pairs(z,1)=cellstr(T.ID(i));
-                name_pairs(z,2)=cellstr(T.ID(j));
-                if(T.Social(i)==T.Social(j) && T.Gmdw(i)==T.Gmdw(j) || ~(T.Social(i)==T.Social(j)) && ~(T.Gmdw(i)==T.Gmdw(j)))
-                    result(z)=1;
-                end
-                z=z+1; 
+function [ result ] = KozossegOsszehasonlit(vector1,vector2)
+    truePositive = 0;
+    falseNegative = 0;
+    trueNegative = 0;
+    falsePositive = 0;
+    for i=1:length(vector1)
+        for j=1:length(vector1)
+            bool1 = (vector1(i)==vector1(j));
+            bool2 = (vector2(i)==vector2(j));
+            if (bool1==bool2)
+                truePositive = truePositive + 1;
+            end
+            if (bool1 == 1 && bool2 == 0)
+                falseNegative = falseNegative + 1;
+            end
+            if (bool1 == 0 && bool2 == 1)
+                trueNegative = trueNegative + 1;
+            end
+            if (bool1 == 0 && bool2 == 1)
+                falsePositive = falsePositive + 1;
             end
             
         end
     end
-
+    result.truePositive = truePositive;
+    result.falseNegative = falseNegative;
+    result.trueNegative = trueNegative;
+    result.falsePositive = falsePositive;
 end
 
